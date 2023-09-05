400286c4bdc2c7495c60d863c3e7f83c1e16155d
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-09-05 12:32:29 +0200

    Color highlighting, next guess, win and loss animation

--- main.js
+++ main.js
@@ -1,75 +1,133 @@
-let numberOfGuesses = 0;
+let currentGuess = 0;
 let currentLetter = 0;
 let guessWord = "";
+let guessDiv;
 let letters = [];
 let word = "";
+const GET_WORD_URL = "https://words.dev-apis.com/word-of-the-day";
+const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word";
+const loss = document.querySelector("footer");
 
 const wordOfToday = async () => {
-    const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
-    const data = await promise.json();
-    console.log(data);
-    return data.word;
+    try {
+        const promise = await fetch(GET_WORD_URL);
+        const data = await promise.json();
+        word = data.word;
+    } catch (error) {
+        console.error(error);
+    }
 };
-
-(async () => {
-    word = await wordOfToday();
-})();
 
 const isLetter = (letter) => {
     return /^[a-zA-Z]$/.test(letter);
 };
 
-const handleInput = (letter) => {
+const isValidWord = async (word) => {
+    const response = await fetch(VALIDATE_WORD_URL, {
+        method: "POST",
+        "content-type": "application/json",
+        body: JSON.stringify({ word: guessWord }),
+    });
+    const data = await response.json();
+
+    return data.validWord;
+};
+
+const handleGuess = () => {
+    logVariables();
+    if (!isValidWord) {
+        // TODO implement validation
+    }
+    if (guessWord === word) {
+        letters.forEach((l) => {
+            l.classList.add("win");
+        });
+    } else {
+        guessWord.split("").forEach((l, i) => {
+            if (!word.includes(l)) {
+                letters[i].classList.add("bg-lightgray");
+            }
+            if (l === word[i]) {
+                console.log(letters[i]);
+                letters[i].classList.add("bg-limegreen");
+            } else if (word.includes(l)) {
+                letters[i].classList.add("bg-yellow");
+            }
+        });
+        currentGuess++;
+        if (currentGuess > 5) {
+            console.log("You lost.");
+            loss.hidden = false;
+            return;
+        }
+        if (guessWord.length >= 5) {
+            currentLetter = 0;
+            guessWord = "";
+        }
+        getGuessDiv();
+    }
+};
+
+const handleKeyDown = (event) => {
+    switch (event.key) {
+        case "Backspace":
+            console.log("Backspace");
+            // TODO implement delete feature
+            break;
+        case "Enter":
+            if (guessWord.length !== 5) {
+                break;
+            }
+            handleGuess();
+            break;
+    }
+};
+
+const handleKeyUp = (event) => {
+    if (!isLetter(event.key)) {
+        return;
+    }
     if (guessWord.length >= 5) {
         return;
     }
-    console.log(letters[currentLetter]);
-    guessWord += letter;
+
+    guessWord += event.key;
     currentLetter++;
+
     if (letters[currentLetter] === undefined) {
         return;
     }
     letters[currentLetter].focus();
 };
 
-(() => {
-    const guessDiv = document.querySelector(`.guess-${numberOfGuesses}`);
+const getGuessDiv = () => {
+    if (currentGuess > 0) {
+        guessDiv.removeEventListener("keydown", handleKeyDown);
+        guessDiv.removeEventListener("keyup", handleKeyUp);
+    }
+
+    guessDiv = document.querySelector(`.guess-${currentGuess}`);
     const lettersNodeList = guessDiv.childNodes;
     const lettersArray = Array.from(lettersNodeList);
-
     letters = lettersArray.filter((letter) => letter.nodeName !== "#text");
     letters[currentLetter].focus();
+    guessDiv.addEventListener("keydown", handleKeyDown);
+    guessDiv.addEventListener("keyup", handleKeyUp);
 
-    guessDiv.addEventListener("keydown", (event) => {
-        switch (event.key) {
-            case "Backspace":
-                console.log("Backspace");
-                break;
-            case "Enter":
-                console.log("Enter");
-                console.log(guessWord, word);
-                if (guessWord.length !== 5) {
-                    break;
-                }
+    return guessDiv;
+};
 
-                if (guessWord === word) {
-                    console.log("You won.");
-                }
-                break;
-            default:
-                if (!isLetter(event.key)) {
-                    event.preventDefault();
-                }
-        }
-    });
+const init = () => {
+    guessDiv = getGuessDiv();
+    wordOfToday();
+};
 
-    guessDiv.addEventListener("keyup", (event) => {
-        const key = event.key;
-        if (!isLetter(key)) {
-            return;
-        }
-        handleInput(event.target.value);
-    });
+init();
 
-    // TODO prevent focus next input if non-letter input
-})();
+const logVariables = () => {
+    console.log("guessWord", guessWord);
+    console.log("currentGuess", currentGuess);
+    console.log("guessdiv", guessDiv);
+    console.log("currentLetter", currentLetter);
+    console.log("letters", letters);
+};
